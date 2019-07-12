cdea97256d3ba5fc35aec9056ac14197ef9520bd
Artur Gevorgyan <artur93gev@gmail.com>
2019-07-12 10:57:41 +0400

    removing unused dummy component

--- src/utils.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Dummy = () => null;
-
-export {
-  Dummy,
-};
--- src/wrapper.js
+++ src/wrapper.js
@@ -3,7 +3,6 @@ import { Router } from 'react-router-dom';
 
 import history from './history';
 import configs from './configs';
-import { Dummy } from './utils';
 
 /* As mentioned in https://github.com/ReactTraining/react-router/issues/3948 issue,
  * for such cases recommended way is to use `Router` instead of `BrowserRouter`
